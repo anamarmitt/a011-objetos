918474e3d315cba41fb9faa2fb5c05ece7709fbc
Ana Marmitt <anap.mmarmitt@gmail.com>
2022-09-05 16:17:53 -0300

    ex2-feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,26 @@
 const filme = {
-	nome: "Auto da Compadecida", 
-	ano: 2000, 
-	elenco: [
-		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
-		"Virginia Cavendish"
-		], 
-	transmissoesHoje: [
-		{canal: "Telecine", horario: "21h"}, 
-		{canal: "Canal Brasil", horario: "19h"}, 
-		{canal: "Globo", horario: "14h"}
-		]
+  nome: "Auto da Compadecida",
+  ano: 2000,
+  elenco: [
+    "Matheus Nachtergaele",
+    "Selton Mello",
+    "Denise Fraga",
+    "Virginia Cavendish",
+  ],
+  transmissoesHoje: [
+    { canal: "Telecine", horario: "21h" },
+    { canal: "Canal Brasil", horario: "19h" },
+    { canal: "Globo", horario: "14h" },
+  ],
 };
 
 // respostas aqui ⬇️
+
+console.log(filme.elenco[0]);
+console.log(filme.elenco[filme.elenco.length - 1]);
+
+console.log(filme.transmissoesHoje);
+
+console.log(filme.transmissoesHoje.find(horario));
+
+console.log(filme.transmissoesHoje[1].horario);
